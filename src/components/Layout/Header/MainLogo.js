73c9f37d441639956955b4b17bcd79014d4a19bc
clemgbld <clementgombauld@hotmail.fr>
2022-01-22 12:07:36 +0100

    perf fixed mobile

--- src/components/Layout/Header/MainLogo.js
+++ src/components/Layout/Header/MainLogo.js
@@ -1,32 +1,13 @@
 import classes from "./MainLogo.module.css";
-import { AnimatedDiv } from "../../Animations/animations";
-
-const divVariants = {
-  initial: {
-    opacity: 0,
-  },
-  animate: {
-    opacity: 1,
-    transition: {
-      duration: 0.3,
-      delay: 0.6,
-    },
-  },
-};
 
 const MainLogo = () => {
   return (
-    <AnimatedDiv
-      variants={divVariants}
-      initial="initial"
-      animate="animate"
-      className={classes.container}
-    >
+    <div className={classes.container}>
       <div className={classes.logo}>
         <ion-icon name="timer-outline"></ion-icon>
       </div>
       <p className={classes.text}>PomoChampion</p>
-    </AnimatedDiv>
+    </div>
   );
 };
 
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,25 +1,8 @@
 import classes from "./Button.module.css";
-import { AnimatedButton } from "../Animations/animations";
-
-const buttonVariants = {
-  initial: {
-    opacity: 0,
-  },
-  animate: {
-    opacity: 1,
-
-    transition: {
-      duration: 0.3,
-    },
-  },
-};
 
 const Button = ({ onClickButton, iconName, textButton, right }) => {
   return (
-    <AnimatedButton
-      variants={buttonVariants}
-      initial="initial"
-      animate="animate"
+    <button
       onClick={onClickButton}
       className={`${classes.button}`}
       style={{ right: right }}
@@ -28,7 +11,7 @@ const Button = ({ onClickButton, iconName, textButton, right }) => {
         <ion-icon name={`${iconName}`}></ion-icon>
       </div>
       <p>{textButton}</p>
-    </AnimatedButton>
+    </button>
   );
 };
 
